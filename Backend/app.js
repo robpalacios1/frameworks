6d47b9a83672c3d62a1352b60818ddcb1922c06c
Roberto <robpalacios1@hotmail.com>
2019-10-17 17:41:53 -0500

    CORS in the API RESTful of NodeJs

--- Backend/app.js
+++ Backend/app.js
@@ -15,6 +15,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // CORS
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 //Anadir prefijo a rutas / cargar rutas
 app.use('/api', article_routes);
